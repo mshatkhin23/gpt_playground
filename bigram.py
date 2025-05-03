e4673b6e0c39c5fba3a98fec647f07b8dbeb2649
mshatkhin23 <mshatkhin23@gmail.com>
2025-05-03 15:33:54 -0400

    cleaning

--- bigram.py
+++ bigram.py
@@ -7,6 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
         super().__init__()
@@ -15,29 +16,25 @@ class BigramLanguageModel(nn.Module):
         self.lm_head = nn.Linear(embedding_dim, vocab_size)
 
     def forward(self, idx, targets=None):
-        embed = self.token_embedding_table(idx) # (B, T, embedding_dim)
-        logits = self.lm_head(embed) # (B, T, vocab_size)
+        embed = self.token_embedding_table(idx)  # (B, T, embedding_dim)
+        logits = self.lm_head(embed)  # (B, T, vocab_size)
         if targets is None:
             loss = None
         else:
             loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), targets.view(-1))
         return logits, loss
-    
+
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
             # get the predictions
             logits, _ = self(idx)
             # focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
+            logits = logits[:, -1, :]  # becomes (B, C)
             # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
+            probs = F.softmax(logits, dim=-1)  # (B, C)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
             # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
         return idx
-
-
-
-
